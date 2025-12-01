2d5df675df52d8543cb8eaa70731eceede248157
yhwHUB <yanghw1902@gmail.com>
2025-12-01 10:07:07 +0900

    도커 이미지 빌드, 푸시 자동화

--- src/main/kotlin/com/example/demo/HomeController.kt
+++ src/main/kotlin/com/example/demo/HomeController.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.RestController
 class HomeController (
     private val s3Service: S3Service
 ){
+
+    @Value("\${custom.secretWord}")
+    private val secretWord: String = ""
+
+
     @GetMapping
     fun main(): String {
-        return "Hi"
+        return "Hi, $secretWord"
     }
 
     @GetMapping("/buckets")
